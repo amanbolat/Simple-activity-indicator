32db5d7810db6e1714e79313c9162384b1dcb363
aman <amanbolat@me.com>
2016-09-15 19:06:48 +0800

    Initial Commit

---
+++ ActivityIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityIndicator
+//
+//  Created by AMANBOLAT BALABEKOV on 15.09.16.
+//  Copyright © 2016 AMANBOLAT BALABEKOV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
